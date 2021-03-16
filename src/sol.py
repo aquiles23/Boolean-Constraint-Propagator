c7a90d3fb6bd68f9a251fb7fa9de592c76565f68
aquiles <jjoseaquiless@gmail.com>
2021-03-16 18:33:56 -0300

    start

---
+++ src/sol.py
@@ -0,0 +1,24 @@
+Var, Claus = input().split()
+Var = int(Var)
+Claus = int(Claus)
+
+dict_var = {x:True for x in range(1,Var+1) }
+list_claus = [[]] * Claus
+
+for x in range(Claus):
+    var_atuais* = input().split()
+    for x in enumerate(var_atuais):
+        var_atuais[x] = int(x)
+    list_claus[x].append(var_atuais)
+
+    
+"""     primeiro = True
+    
+    var_atual = int(input())
+    while var_atual : 
+        if(not primeiro):
+            var_atual = int(input())
+        list_claus[x].append(var_atual)
+        primeiro = False """
+
+
